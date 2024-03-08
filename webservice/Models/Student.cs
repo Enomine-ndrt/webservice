a58195b6af5933fe60aff22d4ce73df5d0e32385
ldelossantos <ldelossantos@geovirtual.mx>
2024-03-07 23:48:56 -0600

    Agregar archivos de proyecto.

---
+++ webservice/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace webservice.Models
+{
+    public class Student
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
